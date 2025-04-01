7ed0f00d53ed0ad0d99801af583ab1bcd7241356
Raihan Hossen <raihanhossen.eee@gmail.com>
2025-04-01 20:04:29 +0600

    data inpput

--- src/Compponents/Book/Book.jsx
+++ src/Compponents/Book/Book.jsx
@@ -5,7 +5,7 @@ const Book = ({vlog}) => {
     return (
         <div>
             <div className=" p-2 ">
-                <p className="text-2xl border border-b-fuchsia-700 rounded-2xl p-2 bg-green-600">{post}</p>
+                <p className="text-2xl border border-b-fuchsia-700 rounded-2xl p-2 bg-green-600 text-white">{post}</p>
             </div>
         </div>
     );
